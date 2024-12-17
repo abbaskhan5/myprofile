f91b97e1b678f489200a03f3f5a29f7f75474503
abbaskhan5 <abbask332321@gmail.com>
2024-12-17 12:58:20 +0500

    my profile

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+import flag from "../assets/pakistan-flag-.jpg";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 2xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>🌟 MERN Stack Developer 🌟</span>
+                    <span className="flex items-center gap-2">
+                        <span>Based</span>
+                        <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In Pakistan</span>
+                    </span>
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                As a skilled MERN Stack Developer based in Pakistan, I specialize in creating dynamic, responsive, and scalable web applications. With expertise in MongoDB, Express.js, React.js, and Node.js, I bring innovative ideas to life through seamless development and efficient coding practices.
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                I am a dedicated MERN Stack Developer from Pakistan, passionate about building full-stack solutions that deliver exceptional performance. By leveraging modern technologies and my deep knowledge of the MERN framework, I create user-friendly applications tailored to meet diverse client needs.
+                </Div>
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                Proudly based in Pakistan, I work as a MERN Stack Developer, focusing on end-to-end development of robust web applications. My expertise in MongoDB, Express.js, React.js, and Node.js ensures cutting-edge solutions that exceed expectations.
+                </Div>
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
